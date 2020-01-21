f401fb78f50998afdfebe41914d0c35abec2cc73
Malcolm Mashig <mjm6jy@virginia.edu>
2020-01-21 17:34:45 -0500

    cleaned percentages

--- 01-import-wrangle.R
+++ 01-import-wrangle.R
@@ -9,3 +9,14 @@ library(here)
 
 fangraphs_raw <- here::here('data', 'fangraphs.csv') %>% 
   read_csv()
+
+fangraphs_clean <- fangraphs_raw %>% 
+  mutate(
+   `FB%` = as.numeric(str_remove(`FB%`, " %")),
+   `SL%` = as.numeric(str_remove(`SL%`, " %")),
+   `CT%` = as.numeric(str_remove(`CT%`, " %")),
+   `CB%` = as.numeric(str_remove(`CB%`, " %")),
+   `CH%` = as.numeric(str_remove(`CH%`, " %")),
+   `SF%` = as.numeric(str_remove(`SF%`, " %")),
+   `KN%` = as.numeric(str_remove(`KN%`, " %"))
+  )
