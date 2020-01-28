5137b0241e1a0ebe075b2d6e8f103031ae4e341c
Malcolm Mashig <mjm6jy@virginia.edu>
2020-01-28 15:30:47 -0800

    1 row/player dataset fangraphs_player

--- 01-import-wrangle.R
+++ 01-import-wrangle.R
@@ -1,10 +1,10 @@
 # install.packages('tidyverse')
 library(tidyverse)
 
-#install.packages('lubridate')
+# install.packages('lubridate')
 library(lubridate)
 
-#install.packages('here')
+# install.packages('here')
 library(here)
 
 fangraphs_raw <- here::here('data', 'fangraphs.csv') %>% 
@@ -19,4 +19,65 @@ fangraphs_clean <- fangraphs_raw %>%
    `CH%` = as.numeric(str_remove(`CH%`, " %")),
    `SF%` = as.numeric(str_remove(`SF%`, " %")),
    `KN%` = as.numeric(str_remove(`KN%`, " %"))
-  )
+  ) %>% 
+  arrange(Name, Season) %>% 
+  group_by(Name) %>% 
+  mutate(experience = row_number()) %>% 
+  ungroup() %>% 
+  select(1:4, 26, 5:25)
+
+# 1 row/player ----------------------------
+
+# xFIP
+season_xfip <- fangraphs_clean %>% 
+  pivot_wider(id_cols = Name, 
+              names_from = Season, 
+              values_from = xFIP, 
+              names_prefix = "xFIP_")
+
+experience_xfip <- fangraphs_clean %>% 
+  pivot_wider(id_cols = Name, 
+              names_from = experience, 
+              values_from = xFIP, 
+              names_prefix = "xFIP_")
+
+# FBv
+
+season_fbv <- fangraphs_clean %>% 
+  pivot_wider(id_cols = Name, 
+              names_from = Season, 
+              values_from = FBv, 
+              names_prefix = "FBv_")
+
+experience_fbv <- fangraphs_clean %>% 
+  pivot_wider(id_cols = Name, 
+              names_from = experience, 
+              values_from = FBv, 
+              names_prefix = "FBv_")
+
+# FB%
+
+season_fbpct <- fangraphs_clean %>% 
+  pivot_wider(id_cols = Name, 
+              names_from = Season, 
+              values_from = `FB%`, 
+              names_prefix = "FB%_")
+
+experience_fbpct <- fangraphs_clean %>% 
+  pivot_wider(id_cols = Name, 
+              names_from = experience, 
+              values_from = `FB%`, 
+              names_prefix = "FB%_")
+
+# Player Data
+fangraphs_player <- bind_cols(season_xfip, season_fbv, season_fbpct,
+                              experience_xfip, experience_fbv, experience_fbpct)
+
+# Remove intermediate data
+remove(season_xfip, season_fbv, season_fbpct,
+       experience_xfip, experience_fbv, experience_fbpct)
+
+  
+  
+  
+  
