b8985b4c2a385cf43f126376c5f0b09b1756618b
Malcolm Mashig <mjm6jy@virginia.edu>
2020-02-25 16:35:59 -0500

    spaghetti

--- spaghetti/spaghetti.R
+++ spaghetti/spaghetti.R
@@ -16,16 +16,25 @@ fangraphs_clean %>%
   mutate(avg_fbv = mean(FBv, na.rm = TRUE),
          avg_fbpctg = mean(`FB%`, na.rm = TRUE),
          avg_ip = mean(IP, na.rm = TRUE),
-         avg_gs = mean(GS, na.rm = TRUE)
+         avg_gs = mean(GS, na.rm = TRUE),
+         mx_experience = max(experience)
          ) %>% 
   ungroup() -> avgs
 
 avgs %>% 
-  filter(experience > 4, Season > 2010) %>% 
+  filter(avg_ip >= 80, IP > 80, mx_experience > 5) %>% 
+  as_tsibble(key = Name, index = experience) %>%
+  ggplot() +
+  geom_line(aes(experience, xFIP, group = Name, color = avg_fbpctg > 60)) +
+  facet_strata(n_strata = 6, along = FBv) +
+  labs(color = "FB Pitcher", title = "IP > 80, avg IP > 80, avg experience > 5")
+
+avgs %>% 
+  filter(mx_experience > 4) %>% 
   as_tsibble(key = Name, index = experience) %>%
   ggplot() +
   geom_line(aes(experience, WAR, group = Name, 
-                color = avg_ip > 160 & avg_gs > 20 & avg_fbv > 90 & avg_fbpctg > 55)) +
+                color = avg_ip > 150 & avg_gs > 20 & avg_fbv > 90 & avg_fbpctg > 50)) +
   facet_sample(n_per_facet = 30, n_facets = 4) +
   labs(color = "")
   # facet_wrap(~(avg_fbpctg > 60))
