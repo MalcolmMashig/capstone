b51243983e6f2f318108a1c0aae60741bb574d34
Jordan Denishgit config --global user.email jfd6twz@virginia.edu <jordandenish@Jordans-MacBook-Air.local>
2020-04-01 22:26:34 -0400

    commit

--- WORKAFTERCORONA/Full Model - JD.R
+++ WORKAFTERCORONA/Full Model - JD.R
@@ -137,27 +137,3 @@ prediColumns <- select(test, c(Name, Season, xFIP) )
 
 RESULTS <- cbind(prediColumns, predictions)
 RESULTS <- drop_na(RESULTS)
-
-
-
-
-#############
-
-# 
-# lda.test <- predict(trainLag12, newdata = test)
-# table(test$chd69,lda.test$class)
-# mean(test$chd69 == lda.test$class)
-# 
-# lda.test$class
-# 
-# table(test$chd69, lda.test$posterior[,2]>0.1)
-# 
-# 
-# preds<-lda.test$posterior[,2]
-# rates<-prediction(preds, test$chd69)
-# roc_result<-performance(rates,measure="tpr", x.measure="fpr")
-# plot(roc_result, main="ROC Curve for Cardio Death")
-# lines(x = c(0,1), y = c(0,1), col="red")
-# auc<-performance(rates, measure = "auc")
-# auc
-
