f0b902357f64c6760e2ca499d5c3f531f1a15b42
Devan Bose <devanbose@Devans-MacBook-Air.local>
2020-04-18 14:48:12 -0400

    p

--- shiny-xfip/url format.R
+++ shiny-xfip/url format.R
@@ -1,3 +1,12 @@
+# install.packages('tidyverse')
+library(tidyverse)
+
+# install.packages('lubridate')
+library(lubridate)
+
+# install.packages('here')
+library(here)
+
 players = future['Name']
 players_clean = vector(mode = "list", length = nrow(players))
 for (i in 1:nrow(players)) {
@@ -11,13 +20,19 @@ fangraphs_urls = 1:length(players_clean)
 for (i in 1:length(players_clean)) {
   fangraphs_urls[i] = paste("https://www.fangraphs.com/players/", players_clean[i], '/', fangraphsids[i,], sep = "")}
 
+mlbdata <- here::here('shiny-xfip', 'PLAYERIDMAP.csv') %>% 
+  read_csv() %>%
+  select(FANGRAPHSNAME,MLBID) 
+
+names(mlbdata)[1] = "Name"
+mlbdata = semi_join(mlbdata, players)
+
+mlbids = mlbdata['MLBID']
+
+mlb_urls = 1:nrow(mlbids)
+for (i in 1:nrow(mlbids)) {
+  mlb_urls[i] = paste("https://securea.mlb.com/mlb/images/players/head_shot/", mlbids[i,], ".jpg", sep = "")}
+
 fangraphs_urls
-
-mlbids = 
-
-mlb_urls = 1:length(mlbids)
-for (i in 1:length(mlbids)) {
-  mlb_urls[i] = paste("https://securea.mlb.com/mlb/images/players/head_shot/", mlbids[i], ".jpg", sep = "")}
-
 mlb_urls
 
