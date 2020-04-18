c9882c1a831a135c999e5bbed26451d1e16d8792
Devan Bose <devanbose@Devans-MacBook-Air.local>
2020-04-18 13:46:34 -0400

    p

--- shiny-xfip/url format.R
+++ shiny-xfip/url format.R
@@ -1,8 +1,18 @@
 players = future['Name']
-players = gsub('\\.','',players)
-players = gsub(' ', '-', players)
-players = tolower(players)
-players
+players_clean = vector(mode = "list", length = nrow(players))
+for (i in 1:nrow(players)) {
+  players_clean[i] = gsub('\\.','',players[i,])
+  players_clean[i] = gsub(' ', '-', players_clean[i])
+  players_clean[i] = tolower(players_clean[i])}
 
 ids = future['playerid']
-ids
+
+players_clean[1]
+ids[1,]
+
+urls = 1:length(players_clean)
+for (i in 1:length(players_clean)) {
+  urls[i] = paste("https://www.fangraph.com/players/", players_clean[i], '/', ids[i,], sep = "")
+}
+
+urls
