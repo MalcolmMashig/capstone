042e7a3da6f737cc6be18584e817306abe3407cd
Malcolm Mashig <mjm6jy@virginia.edu>
2020-04-22 12:50:35 -0400

    fixy

--- shiny-xfip/app.R
+++ shiny-xfip/app.R
@@ -259,8 +259,8 @@ server <- function(input, output) {
       t %>% 
         ggplot() +
         geom_line(aes(year, xFIP, linetype = year > 2019), size = 2.5) +
-        geom_hline(aes(yintercept=leagueAverageXFIP, color = "red")) +
-        geom_text(aes(2020,leagueAverageXFIP, hjust = 2, vjust = -1, label = "League Average XFIP", color = "red")) +
+        geom_hline(aes(yintercept=leagueAverageXFIP %>% as.numeric(), color = "red")) +
+        geom_text(aes(2020,leagueAverageXFIP %>% as.numeric(), hjust = 2, vjust = -1, label = "League Average XFIP", color = "red")) +
         geom_point(aes(year, xFIP), size = 5, color = "maroon") +
         theme_linedraw() + 
         theme(legend.position = "none",
